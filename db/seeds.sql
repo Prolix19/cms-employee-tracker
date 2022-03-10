47b94aa08f6c7250eb8fea660a6d3e0e0e86998d
Prolix <51418825+Prolix19@users.noreply.github.com>
2022-03-10 00:42:22 -0600

    Create file structure, schema and seeds; install packages

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- INSERT INTO department (name) VALUES ('Sales'), ('Finance'), ('IT'), ('HR')
+
+-- INSERT INTO role (title, salary, department_id) VALUES ('Sales Engineer', 75000, 1)
+
+-- INSERT INTO employee (first_name, last_name, role_id) VALUES ("John", "Doe", 1)
